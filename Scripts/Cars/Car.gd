fbfbcfe1d51071b9935f0bf4fe129846f24f7881
skoggy <erikskoglund88@gmail.com>
2022-08-11 17:01:22 +0200

    added rolls and boost

--- Scripts/Cars/Car.gd
+++ Scripts/Cars/Car.gd
@@ -6,14 +6,19 @@ onready var wheel_back_left: Wheel = $Car/BottomPlate/Wheel_Back_Left
 onready var wheel_back_right: Wheel = $Car/BottomPlate/Wheel_Back_Right
 
 export var max_steer_angle_degrees: float = 35.0
-export var steer_speed: float = 15.0
 export var acceleration: float = 50.0
 export var jump_force: float = 10.0
 export var air_drag: float = 0.1
 export var angular_damping: float = 10
+export var roll_speed: float = 50.0
+export var ground_steer_speed: float = 25.0
+export var boost_force: float = 5.0
 
-var steering: float = 0.0
-var input_steering: float = 0.0
+var left_right: float = 0.0
+var up_down: float = 0.0
+var roll: float = 0.0
+var boost: bool = false
+
 var input_gas: float = 0.0
 var input_jump: bool = false
 
@@ -27,10 +32,6 @@ func _ready():
 	wheels.append(wheel_back_left)
 	wheels.append(wheel_back_right)
 
-func integrate_steering(delta):
-	steering = lerp(steering, input_steering, steer_speed * delta)
-	steering = clamp(steering, -1.0, 1.0)
-
 func rotate_wheels_lateral(radians):
 	wheel_front_left.rotate_lateral(radians)
 	wheel_front_right.rotate_lateral(radians)
@@ -43,40 +44,66 @@ func steer(degrees):
 
 func jump():
 	apply_impulse(Vector3.ZERO, transform.basis.y * jump_force * mass)
-
-func _process(delta):
-	input_steering = Input.get_axis("steer_right", "steer_left")
-	input_gas =	Input.get_axis("reverse", "gas")
-	input_jump = Input.is_action_just_pressed("jump")
-
+	
+func update_state():
 	wheels_grounded = 0
 	for wheel in wheels:
 		if wheel.grounded:
 			wheels_grounded += 1
 
 	all_wheels_grounded = wheels_grounded == 4
-		
-	integrate_steering(delta)
-	steer(steering * deg2rad(max_steer_angle_degrees))
+
+func _process(_delta):
+	left_right = Input.get_axis("right", "left")
+	up_down = Input.get_axis("back", "forward")
+	roll = Input.get_axis("roll_left", "roll_right")
+	input_gas =	Input.get_axis("reverse", "gas")
+	input_jump = Input.is_action_just_pressed("jump")
+	boost = Input.is_action_pressed("boost")
+	
+	left_right = clamp(left_right, -1.0, 1.0)
+	up_down = clamp(up_down, -1.0, 1.0)
+	roll = clamp(roll, -1.0, 1.0)
+
+	update_state()
+	steer(left_right * deg2rad(max_steer_angle_degrees))
 	if input_jump and wheel_front_left.grounded:
 		jump()
 
-func _physics_process(_delta):
+func _integrate_forces(state):
 	if all_wheels_grounded:
 		# acceleration
-		add_central_force(transform.basis.z * input_gas * acceleration * mass)
+		state.add_central_force(transform.basis.z * input_gas * acceleration * mass)
 		# wheel lateral forces
-		apply_lateral_force()
+		apply_lateral_force(state)
+		
+	if not all_wheels_grounded:
+		apply_air_rolls(state)
 
-	# steering
-	add_torque(Vector3(0, steering * deg2rad(max_steer_angle_degrees) * 50.0 * mass, 0))
+	if all_wheels_grounded:
+		# steering
+		state.add_torque(transform.basis.y * left_right * ground_steer_speed * mass)
+
 	# air drag
-	add_central_force(-linear_velocity * air_drag * mass)
+	state.add_central_force(-state.linear_velocity * air_drag * mass)
 	# torque damping
-	add_torque(-angular_velocity * angular_damping * mass)
+	state.add_torque(-state.angular_velocity * angular_damping * mass)
+	# boost
+	if boost:
+		state.add_central_force(transform.basis.z * boost_force * mass)
+	
 
-func apply_lateral_force():
+func apply_air_rolls(state):
+	# roll
+	state.add_torque(transform.basis.z * roll * roll_speed * mass)
+	# pitch
+	state.add_torque(transform.basis.x * up_down * roll_speed * mass)
+	# yaw
+	state.add_torque(transform.basis.y * left_right * roll_speed * mass)
+	
+
+func apply_lateral_force(state):
 	# TODO: Per wheel lateral force?
-	var velocity_direction = linear_velocity.normalized()
+	var velocity_direction = state.linear_velocity.normalized()
 	var dot = transform.basis.x.dot(velocity_direction)
-	add_central_force(-transform.basis.x * dot * linear_velocity.length() * mass * 20.0)
+	state.add_central_force(-transform.basis.x * dot * state.linear_velocity.length_squared() * mass)
